a562c7dfe9fccde72efcd70ee250fdd3f947f953
SylvainBoudacher <sylvain.boudacher@gmail.com>
2024-04-19 16:41:18 +0200

    style(page): add background component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import BackgroundCliq from "@/components/backgroundLogoCliq";
+
 export default function Home() {
   return (
     <main className="min-h-screen w-full h-full">
       <div className="relative mb-4 flex items-center justify-center py-[26vh] pt-[18vh] sm:pt-[26vh] h-full">
-        {/* CREATE absolute Background */}
-        <p>background</p>
+        <BackgroundCliq />
 
         <div>
           {/* Create ->  Title */}
