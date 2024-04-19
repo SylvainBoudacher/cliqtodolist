abc2c0c3da45b03bec98041d3e77b4d86a548fdd
SylvainBoudacher <sylvain.boudacher@gmail.com>
2024-04-19 16:30:50 +0200

    style(page): setup plan

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <main className="min-h-screen w-full h-full">
-      <p>pouet</p>
+      <div className="relative mb-4 flex items-center justify-center py-[26vh] pt-[18vh] sm:pt-[26vh] h-full">
+        {/* CREATE absolute Background */}
+        <p>background</p>
+
+        <div>
+          {/* Create ->  Title */}
+          {/* Create -> Input add to list */}
+          {/* Create -> List items */}
+        </div>
+      </div>
     </main>
   );
 }
